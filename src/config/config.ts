ad9d55ce97b2ff82eb0bd897ed32257ebb068372
Le Minh <minhld@aiacademy.edu.vn>
2023-10-11 13:57:17 +0700

    test docker

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   port: process.env.PORT || 4010,
   db: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "0.0.0.0",
     port: 3306,
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || "Minh2000@",
