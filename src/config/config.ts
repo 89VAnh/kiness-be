3b6d41e0e17811dec7e4b3169f09a7e82b307c7b
Le Minh <minhld@aiacademy.edu.vn>
2023-10-25 22:00:12 +0700

    changed env

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ export const config = {
   limit_size: process.env.LIMIT_SIZE || 3145728,
   db: {
     host: process.env.DB_HOST || "118.70.52.237",
-    port: 3306,
+    port: 3303,
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || "aiacademy@",
     database: process.env.DB_NAME || "kiness-dev",
