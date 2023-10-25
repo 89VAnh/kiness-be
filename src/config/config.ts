5f87d0654952b9fe8a4a9dc3f3ce818da751d058
Le Minh <minhld@aiacademy.edu.vn>
2023-10-25 23:18:45 +0700

    fixed config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,10 @@ export const config = {
   port: process.env.PORT || 4010,
   limit_size: process.env.LIMIT_SIZE || 3145728,
   db: {
-    host: process.env.DB_HOST || "118.70.52.237",
-    port: 3303,
+    host: process.env.DB_HOST || "3.24.161.147",
+    port: 3306,
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "aiacademy@",
+    password: process.env.DB_PASSWORD || "Minh2000@",
     database: process.env.DB_NAME || "kiness-dev",
   },
   jwt: {
