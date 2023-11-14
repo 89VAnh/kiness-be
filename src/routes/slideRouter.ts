338a8c67dabf0f246a1bf99abfa5e002919cdb53
minhld3 <minhld@aiacademy.edu.vn>
2023-11-14 14:08:18 +0700

    fixed slide

--- src/routes/slideRouter.ts
+++ src/routes/slideRouter.ts
@@ -10,5 +10,5 @@ slideRouter.get('/dropdown', slideController.getSlideDropdown.bind(slideControll
 slideRouter.post('/create', authenticate, slideController.createSlide.bind(slideController));
 slideRouter.post('/update', authenticate, slideController.updateSlide.bind(slideController));
 slideRouter.post('/delete', authenticate, slideController.deleteSlide.bind(slideController));
-slideRouter.post('/search', authenticate, slideController.searchSlide.bind(slideController));
+slideRouter.post('/search', slideController.searchSlide.bind(slideController));
 export default slideRouter;
