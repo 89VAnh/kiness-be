d1b546f2530e4f2c16970932a5abc02e3886e323
89VAnh <vietanha9a2@gmail.com>
2023-12-26 17:19:48 +0700

    upgrade video

--- src/models/video.ts
+++ src/models/video.ts
@@ -3,7 +3,7 @@ import { BaseModel } from "./base";
 export interface Video extends BaseModel {
   video_id: number;
   video_name: string;
-  video_link: string;
+  video_code: string;
   is_foreign: boolean;
 }
 
